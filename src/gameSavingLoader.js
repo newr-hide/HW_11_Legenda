bf23b6af1c4ae4b2d8f8be7a2679361e8047c87d
Bondarev Artem <bond210484@mail.ru>
2025-05-13 08:57:21 +0700

    add 1 task

--- src/gameSavingLoader.js
+++ src/gameSavingLoader.js
@@ -1,18 +1,40 @@
 import json from "./parser.js";
 import read from "./reader.js";
 
-export default class GameSavingLoader {
-    static async load() {
-        try {
-            const rawData = await read();
-            const parseString = await json(rawData);
-
-            const gameSavingObject = JSON.parse(parseString);
-            return gameSavingObject;
-        } catch (error) {
-            throw error;
-            
-        }
+class GameSaving {
+    constructor(data) {
+      this.id = data.id;
+      this.created = data.created;
+      this.userInfo = data.userInfo;
     }
   }
+
+export default class GameSavingLoader {
+    static load() {
+      return read()
+        .then(fileData => json(fileData))
+        .then(stringifiedData => JSON.parse(stringifiedData)) 
+        .then(parsedData => new GameSaving(parsedData)) 
+        .catch(error => { throw error }); 
+    }
+  }
+
+  GameSavingLoader.load()
+  .then(saving => console.log('Загружено:', saving))
+  .catch(error => console.error('Ошибка загрузки:', error));
+
+// export default class GameSavingLoader {
+//     static async load() {
+//         try {
+//             const rawData = await read();
+//             const parseString = await json(rawData);
+
+//             const gameSavingObject = JSON.parse(parseString);
+//             return gameSavingObject;
+//         } catch (error) {
+//             throw error;
+            
+//         }
+//     }
+//   }
 
